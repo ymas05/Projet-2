573fcf7dbc4bcf5488a7bdb2ad1a6e120023bf10
ymas05 <ymas.batonon@polymtl.ca>
2025-04-13 21:52:52 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+# Constants
+WIDTH, HEIGHT = 600, 600
+BOARD_SIZE = 8
+SQUARE_SIZE = WIDTH // BOARD_SIZE
+WHITE = (238, 238, 210)
+GRAY = (119,136,153)
+HIGHLIGHT = (186, 202, 68)
+
+# Load Images
+PIECE_IMAGES = {'white_pawn':'p', 
+                'white_rook':'r', 
+                'white_knight':'n', 
+                'white_bishop':'b', 
+                'white_queen':'q', 
+                'white_king':'k', 
+                'black_pawn':'P', 
+                'black_rook':'R', 
+                'black_knight':'N', 
+                'black_bishop':'B', 
+                'black_queen':'Q', 
+                'black_king':'K'}
+
+
